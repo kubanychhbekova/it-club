8c910c88afff893799eb8dad08dd3db957587de4
kubanychhbekova <a7966519@gmail.com>
2022-10-23 20:13:08 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
-const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
+
+// const swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 2,
+//     spaceBetween: 300,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//
+//     },
+// });
+
+
+
+
+
+const swiper = new Swiper (".mySwiper", {
+    loop:true,
     pagination: {
         el: ".swiper-pagination",
-        clickable: true,
     },
-});
+    slidesPerView: 1,
 
+    breakpoints: {
+        '575': {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+    },
+        '767': {
+            slidesPerView: 2,
+            spaceBetween: 100,},
 
+        '360': {
+            slidesPerView: 2,
+            spaceBetween: 100,},
 
+    }
 
-
-
+)
